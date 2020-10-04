ac24c4000866b5a2873f42dd44abfbe6b02bae38
ogulcankirtay <56118583+ogulcankirtay@users.noreply.github.com>
2020-10-04 13:01:09 +0300

    Add files via upload

---
+++ fuctions.py
@@ -0,0 +1,24 @@
+# # # # # def sayhello(n):
+# # # # #     print(f"hello {n}")
+
+# # # # # sayhello("oğulcan")
+# # # # # sayhello("abc")
+# # # # def sayhello(n="user"):
+# # # #     print(f"hi {n}")
+
+# # # # sayhello("oğulcan")
+# # # # sayhello("abc")
+# # # # sayhello()
+# # # def sayhello(n="user"):
+# # #     return "hello "+n
+# # # msg=sayhello("oğulcan")
+# # # print(msg)
+# # def total(num1,num2):
+# #     return num1+num2
+# # toplam=total(10,20)
+# # print(toplam)
+# def toplam(a):
+#     a=10+20
+# a=10
+# toplam(a)
+# print(a)
